c5537cdcd7a2005a515a2ad6586b94544f94f05e
Alejandro <alejandrolujan1999@outlook.es>
2024-11-08 02:45:25 +0000

    TodoList

---
+++ src/js/component/TodoList.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+//Estados para las tareas
+const TodoList = () => {
+    const [tarea, setTarea] = useState("");
+    const [tareas, setTareas] = useState([]);
+
+//Función flecha para añadir tareas
+    const addTarea = (newTask) => {
+        setTareas([...tareas, newTask])
+    }
+
+//Función flecha para eliminar tareas
+    const deleteTarea = (index) => {
+        const newTasks = tareas.filter((_, i) => i !== index);
+        setTareas(newTasks);
+    };
+
+    return (
+        <div className="App">
+            <div>
+                <h1>Lista de tareas:</h1>
+                <input className="escribir" type="text" placeholder="Añadir una nueva tarea..."
+                onChange={(e) => {setTarea(e.target.value)}}
+                value={tarea} onKeyDown={ (e) => {
+                    if (e.key === "Enter") {
+                        addTarea(tarea);
+                        setTarea("");
+                    }
+                }}
+                />
+            </div>
+            <div className="lista">
+                <ul>
+                    {
+                    tareas.map( (task, index) => (
+                        <li key={index} className="task-item">• {task} 
+                            <button className="delete-btn"
+                            onClick={() => 
+                            deleteTarea(index)}>x
+                            </button>
+                        </li>
+                        ))
+                    }
+                    <li className="contarTareas">
+                        {tareas.length === 0 ? "No hay tareas pendientes" :
+                        `Tienes ${tareas.length} tarea${tareas.length > 1 ? 's' : ''}
+                        pendiente${tareas.length > 1 ? 's' : ''}`}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default TodoList;
